b69763a430b4bf4ec9cfbc89079b0aa38329c491
OrangeInky <86501049+OrangeInky@users.noreply.github.com>
2025-01-31 13:18:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -4,11 +4,26 @@ function stringRev(str) {
 
 function digitRev(num) {
     str = num.toString();
-    return num + Number(stringRev(str))
+    return Number(stringRev(str));
+}
+
+function addDigitRev(num) {
+    return num+BigInt(digitRev(num));
 }
 
 function isPalindrome(num) {
-        
+    return num==digitRev(num);
 }
 
-console.log(digitRev(19))
+function palindromeQuest(seed=Math.floor(Math.random()*10),it=1000,counter=0) {
+    if (it>0 && !isPalindrome(seed)) {
+        console.log(seed);
+        counter+=1
+        palindromeQuest(addDigitRev(seed),it-1,counter);
+    } else {
+        console.log(seed)
+        console.log(`It took ${counter} steps to reach a palindrome! ${seed} is not a Lychrel Number!`)
+    }
+}
+
+palindromeQuest(BigInt(197),100)
